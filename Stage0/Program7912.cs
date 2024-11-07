f18a8967e21f117dc41b176d0cd55ef6d401403b
ouriel mimoun <mimounouriel@gmail.com>
2024-11-07 18:24:04 +0200

    Welcome7912

---
+++ Stage0/Program7912.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial class Program7912
+    {
+        static partial void Welcome7912()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
