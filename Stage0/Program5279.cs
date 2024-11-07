05a4f6c2e0cd19567d88c240ba3a33c07e567058
HLOON <eliehadad770@gmail.com>
2024-11-07 17:15:30 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program5279.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome5279();
+            Welcome7192();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome7192();
+        private static void Welcome5279()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine() ?? "";
+            Console.WriteLine("{0}, welcome to my first console aplication", userName);
+        }
+    }
+}
