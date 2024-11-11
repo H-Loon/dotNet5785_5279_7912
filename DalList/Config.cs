7b422c782e9267ef59b131a01d138af05222ca8d
ouriel mimoun <mimounouriel@gmail.com>
2024-11-11 23:24:25 +0200

    config.1

--- DalList/Config.cs
+++ DalList/Config.cs
@@ -1,6 +1,8 @@
 ﻿namespace Dal;
 internal static class Config
 {
-
+    internal const int startCallId = 1;
+    private static int nextCallId
+        internal static int NextCallId => nextCallId++;
 }
 
