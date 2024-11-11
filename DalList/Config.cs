304bb4f247f10ddb1fab75d23be569afabe8fadd
ouriel mimoun <mimounouriel@gmail.com>
2024-11-12 00:19:49 +0200

    config.2

--- DalList/Config.cs
+++ DalList/Config.cs
@@ -1,8 +1,31 @@
 ﻿namespace Dal;
 internal static class Config
 {
-    internal const int startCallId = 1;
+    // Id number for the next new read 
+    internal const int startCallId = 1; 
     private static int nextCallId = startCallId;
     internal static int NextCallId => nextCallId++;
+
+    // Id number Assignment
+    internal const int startAssignmementId = 1;
+    private static int nextAssignmementId = startAssignmementId;
+    internal static int NextAssignmementId => nextAssignmementId++;
+
+    // System clock editable 
+    internal static DateTime Clock { get; set; } = DateTime.Now;
+
+    // Period risk
+    internal static TimeSpan RiskRange = TimeSpan.FromHours(1);// previent du danger 1h avant 
+
+    // Reset all entity
+    internal static void Reset()
+    {
+        nextCallId = startCallId;
+        nextAssignmementId = startAssignmementId;
+
+        Clock = DateTime.Now;
+        RiskRange = TimeSpan.FromHours(1);
+
+    }
 }
 
