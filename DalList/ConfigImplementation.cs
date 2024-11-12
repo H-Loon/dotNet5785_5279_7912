fd7f77c9d662cc34350b1e88346b148f430d28ed
ouriel mimoun <mimounouriel@gmail.com>
2024-11-12 20:00:01 +0200

    modif

--- DalList/ConfigImplementation.cs
+++ DalList/ConfigImplementation.cs
@@ -8,13 +8,13 @@ public class ConfigImplementation : IConfig
         get => Config.Clock; // qd on demande l'h , on va la chercher ds Config
         set => Config.Clock = value; // on modifie l'h ds Config ; la new heure sera value 
     }
-    public void Reset()
-     { 
-        Config.Reset(); 
-    }
     public TimeSpan RiskRange 
     {
         get => Config.RiskRange;
         set => Config.RiskRange = value;
-    } 
+    }
+    public void Reset()
+    {
+        Config.Reset();
+    }
 }
