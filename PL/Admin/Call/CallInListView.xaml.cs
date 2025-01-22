3ca0e0ff7c25b489147643e3dbb9a9cb5ebb7e38
ouriel mimoun <mimounouriel@gmail.com>
2025-01-22 20:02:21 +0200

    .

--- PL/Admin/Call/CallInListView.xaml.cs
+++ PL/Admin/Call/CallInListView.xaml.cs
@@ -149,8 +149,8 @@ namespace PL.Admin.Call
             set
             {
                 _selectedCall = value;
-                //if (value != null)
-                //    CallInfo = new CallInfoView(value.CallId);
+                if (value != null)
+                    CallInfo = new CallInfoView(value.CallId);
             }
         }
         private BO.CallInListField _callInListFieldFiltred = BO.CallInListField.None;
